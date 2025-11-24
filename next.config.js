b2427bcb2f9f86807afd354c629f4e5ffaebc55c
becodewala-youtube <kvikash74673@gmail.com>
2025-11-24 13:09:40 +0530

    npm run build error fixing on nextjs config to deploye duiring vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: { unoptimized: true },
+  reactStrictMode: true, // optional but recommended
 };
 
 module.exports = nextConfig;
